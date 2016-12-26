54d2ec58ff1d512ea05abedafcb67b164ec40f77
lian <caryuio2@gmail.com>
2016-12-26 16:29:48 +0800

    uploading a file

---
+++ cake_web/system/loginOK.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #E4CDAB;
+	background-image: url(../image/jyghkjghk.jpg);
+}
+.style1 {color: #666633}
+.style4 {color: #666633; font-size: medium; }
+.style5 {color: #888888}
+-->
+</style></head>
+
+<body>
+<div align="center">
+  <p>&nbsp;</p>
+  <p class="style4"><span class="style5">-管理者登入成功</span>-</p>
+  <p class="style1">&nbsp; </p>
+  <p class="style1">&nbsp;</p>
+</div>
+</body>
+</html>
