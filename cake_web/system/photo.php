a292e7292a40fcfffae3cb66945460c87b40a1a2
lian <caryuio2@gmail.com>
2016-12-26 16:29:47 +0800

    uploading a file

---
+++ cake_web/system/photo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>[查看圖片]</title>
+</head>
+
+<body>
+<div align="center">[查看圖片]<br />
+<img src="store_photo/<?php echo $_GET['p']?>" width="417" height="293" /><br />
+<a href="javascript:close()">關閉本視窗</a> </div>
+</body>
+</html>
