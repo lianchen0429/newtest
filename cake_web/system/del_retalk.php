0d310159ceb2d9dca37e9b8b134362748596d769
lian <caryuio2@gmail.com>
2016-12-26 16:29:49 +0800

    uploading a file

---
+++ cake_web/system/del_retalk.php
@@ -0,0 +1,60 @@
+<?php require_once('../Connections/easyshop.php'); ?>
+<?php
+session_start();
+if($_SESSION['MM_Username']<>"admin"){
+exit;
+}
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+if ((isset($_GET['id'])) && ($_GET['id'] != "")) {
+  $deleteSQL = sprintf("DELETE FROM re_talk WHERE re_id=%s",
+                       GetSQLValueString($_GET['id'], "int"));
+
+  mysql_select_db($database_easyshop, $easyshop);
+  $Result1 = mysql_query($deleteSQL, $easyshop) or die(mysql_error());
+
+  $deleteGoTo = "s_talk";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $deleteGoTo .= (strpos($deleteGoTo, '?')) ? "&" : "?";
+    $deleteGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $deleteGoTo));
+}
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <div align="center">
+    <input type="submit" name="Submit" value="送出" />
+  </div>
+</form>
+</body>
+</html>
