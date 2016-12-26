d602281f5329f3344219be22ba409f950d7433a3
lian <caryuio2@gmail.com>
2016-12-26 16:29:46 +0800

    uploading a file

---
+++ cake_web/system/system.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>系統管理</title>
+</head>
+
+<frameset rows="*" cols="110,*" framespacing="0" frameborder="NO" border="0">
+  <frame src="r_system.php" name="leftFrame" scrolling="NO" noresize>
+  <frame src="loginOK.php" name="mainFrame">
+</frameset>
+<noframes><body>
+</body></noframes>
+</html>
