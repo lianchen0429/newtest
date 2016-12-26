43a81e1e6d0aafd8fab0433933f925a5288a8caa
lian <caryuio2@gmail.com>
2016-12-26 16:30:09 +0800

    uploading a file

---
+++ cake_web/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=big5" />
+<title>�J�|�ʪ���</title>
+</head>
+
+<frameset rows="*" cols="140,*" framespacing="0" frameborder="no" border="0">
+  <frame src="left.php" name="leftFrame" scrolling="No" noresize="noresize" id="leftFrame" />
+  <frame src="right.php" name="mainFrame" id="mainFrame" />
+</frameset>
+<noframes><body>
+</body>
+</noframes></html>
