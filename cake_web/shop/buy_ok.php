1c9a49e133c8b2c261ce5bbac819cba8c68d0fc2
lian <caryuio2@gmail.com>
2016-12-26 16:28:42 +0800

    uploading a file

---
+++ cake_web/shop/buy_ok.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>購買完畢</title>
+<style type="text/css">
+<!--
+.style1 {color: #CC3300}
+.style2 {font-weight: bold}
+.style3 {color: #CC3300; font-weight: bold; }
+-->
+</style>
+</head>
+
+<body>
+<div align="center"><span class="style3">[購買完畢]</span>
+  <?php require_once('buy_top.php'); ?><hr>
+</div>
+</body>
+</html>
