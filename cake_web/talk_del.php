30b603aa3e30248cf65c32e343d3eadedf72fb1b
lian <caryuio2@gmail.com>
2016-12-26 16:30:04 +0800

    uploading a file

---
+++ cake_web/talk_del.php
@@ -0,0 +1,54 @@
+<?php require_once('Connections/cka_table_db.php'); ?>
+<?php
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+if ((isset($_GET['t_id'])) && ($_GET['t_id'] != "")) {
+  $deleteSQL = sprintf("DELETE FROM talkall WHERE t_id=%s",
+                       GetSQLValueString($_GET['t_id'], "int"));
+
+  mysql_select_db($database_cka_table_db, $cka_table_db);
+  $Result1 = mysql_query($deleteSQL, $cka_table_db) or die(mysql_error());
+
+  $deleteGoTo = "talkall.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $deleteGoTo .= (strpos($deleteGoTo, '?')) ? "&" : "?";
+    $deleteGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $deleteGoTo));
+}
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <input type="submit" name="Submit" value="送出" />
+</form>
+</body>
+</html>
